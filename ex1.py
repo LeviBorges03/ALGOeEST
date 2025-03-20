05d1e39fb9b993261d6d4643f1e7e57b3adabfcc
Levi Borges Ferreira <borgesferreiralevi@gmail.com>
2025-03-20 14:44:58 -0400

    Add files via upload

---
+++ ex1.py
@@ -0,0 +1,1 @@
+print=input("Bem vindo ao mundo Python")
---
+++ index.py
@@ -0,0 +1,13 @@
+# #isso é um comentario
+# # print("mensagem")
+# idade=6
+# altura=0.50
+# peso=5.950
+# nome="Francisco"
+# print(idade,altura,peso,nome)lreg
+
+
+nome=input("insira seu nome") #input texto
+idade=int(input("insira idade")) #int numero inteiro
+peso=float(input("insira seu peso")) #numero real
+print(f" nome, {nome} idade, {idade} anos, {peso} kg") #mostar para o usuario
