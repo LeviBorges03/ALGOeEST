021920eb57c2685f58a8eea60e1f431cdf0c7e18
Levi Borges Ferreira <borgesferreiralevi@gmail.com>
2025-03-27 16:43:27 -0400

    Add files via upload

---
+++ ex8.py
@@ -0,0 +1,6 @@
+usuario = input("Insira seu usuário")
+senha = input("Insira sua senha")
+if usuario == "Levi" and senha == "1234":
+    print(f"Bem vindo {usuario}!")
+else:
+    print("Acesso negado!")
